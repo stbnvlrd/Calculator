03013441e907560403da118452e042efd39b4897
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-05-17 11:48:07 -0500

    Basic addition function

--- calculator.js
+++ calculator.js
@@ -33,3 +33,31 @@ numbers.forEach((item) => { item.addEventListener('click', newNum)});
 backspace.addEventListener('click', erase)
 clear.addEventListener('click', clearScreen)
 
+
+let operation = 0;
+let memoryNumber = 0;
+
+// if operation == 0 no operation has been selected
+// if operation == 1 addition operation has been selected
+// if operation == 2 subtraction operation has been selected
+// if operation == 3 multiplication operation has been selected
+// if operation == 4 division operation has been selected
+
+
+const additionOperation = function(){
+    memoryNumber = parseInt(screentext.innerHTML);
+    oldscreentext.innerHTML =  screentext.innerHTML + " + ";
+    screentext.innerHTML = "";
+    operation = 1;
+}
+addition.addEventListener('click', additionOperation);
+
+const equalOperation = function(){
+    oldscreentext.innerHTML = oldscreentext.innerHTML + screentext.innerHTML;
+    screentext.innerHTML = memoryNumber + parseInt(screentext.innerHTML);
+    operation = 0;
+}
+
+equal.addEventListener('click', equalOperation); 
+
+
