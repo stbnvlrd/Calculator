1c09f44c4464a54c35e1649756d5bdcf768e6109
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-05-17 11:46:46 -0500

    Adding AC and backspace function to the calculator

--- calculator.js
+++ calculator.js
@@ -4,21 +4,32 @@ const percentage = document.getElementById("percentage");
 const division = document.getElementById("division");
 const multiplicacion = document.getElementById("multiplication");
 const subtraction = document.getElementById("subtraction");
-const addittion = document.getElementById("addition");
-const equal = document.getElementById("point");
-const point = document.getElementById("equal");
+const addition = document.getElementById("addition");
+const point = document.getElementById("point");
+const equal = document.getElementById("equal");
 const screentext = document.getElementById("newnumber");
 const oldscreentext = document.getElementById("oldnumber");
-
 
 const newNum = function(){
     screentext.innerHTML =  screentext.innerHTML + this.innerHTML;
 }
 
+const erase = function(){
+    screentext.innerHTML = screentext.innerHTML.slice(0,-1);
+}
+
+const clearScreen = function(){
+    if(screentext.innerHTML === ""){
+        oldscreentext.innerHTML = ""
+    }else{
+        screentext.innerHTML = ""
+    };
+}
+
 var numbers = document.querySelectorAll('div.numberClass');
-console.log(numbers);
+
 numbers.forEach((item) => { item.addEventListener('click', newNum)});
 
-
-
+backspace.addEventListener('click', erase)
+clear.addEventListener('click', clearScreen)
 
